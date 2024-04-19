6cd58961850803e11e1db0659ba8f697e27f1686
root <root@LAPTOP-P7UDNGNK>
2024-04-19 16:05:44 +0000

    problema con estrai dovuto a cmp che era sbagliato ora cambiato upper con lower anche in col e va

--- Matrix.cpp
+++ Matrix.cpp
@@ -9,75 +9,62 @@ Matrix<T,S>::Matrix(std::map<std::array<std::size_t,2>,T,cmp<S>> D):Dati(D){}
 //call operator non const(return reference per poter modificare)
 template <class T, StorageOrdering S>
 T& Matrix<T,S>::operator() (std::size_t i,std::size_t j){
-    if(S==StorageOrdering::row){
-        if (is_compress()){
-            for(unsigned int jj=RowPoint[i]; jj<RowPoint[i+1]; jj++){
-                if(ColIndx[jj]==j)
-                    return val[jj];    //ritorna ref a vettore di valori in posizione i j 
-            } 
-            // Da fare: ERRORE SE INDICI NON PRESENTI IN MATRIX   
-            //this->uncompress();           questo uncompress() rida valore non zero da modificare ma lascia matrice uncompress manca rimetterla compress 
-            }   
-        else 
-            return Dati[{i,j}];        // cosi se poszione(i,j) non presente viene aggiunta  
+    if (is_compress()){
+        if constexpr(S==StorageOrdering::row){
+                for(unsigned int jj=RowPoint[i]; jj<RowPoint[i+1]; jj++){
+                    if(ColIndx[jj]==j)
+                        return val[jj];    //ritorna ref a vettore di valori in posizione i j 
+                } 
+                // Da fare: ERRORE SE INDICI NON PRESENTI IN MATRIX   
+        }
+        if constexpr(S==StorageOrdering::col){
+                for(unsigned int jj=RowPoint[j]; jj<RowPoint[j+1]; jj++){
+                    if(ColIndx[jj]==i)
+                        return val[jj];    //ritorna ref a vettore di valori in posizione i j 
+                }
+                // DA Fare: ERRORE SE INDICI NON PRESENTI IN MATRIX 
+        }
     }
-    if(S==StorageOrdering::col){
-        if (is_compress()){
-            for(unsigned int jj=RowPoint[j]; jj<RowPoint[j+1]; jj++){
-                if(ColIndx[jj]==i)
-                    return val[jj];    //ritorna ref a vettore di valori in posizione i j 
-            } // DA Fare: ERRORE SE INDICI NON PRESENTI IN MATRIX     
-        } 
-        else{
-            return Dati[{i,j}];}        // cosi se poszione(i,j) non presente viene aggiunta 
-
-    }
-}
+    return Dati[{i,j}];        // cosi se poszione(i,j) non presente viene aggiunta
+} 
 
 // call operator const (leave user look for i,j in range of matrix)
 template <class T,StorageOrdering S>
 T Matrix<T,S>::operator() (std::size_t i,std::size_t j)const{
-    if(S==StorageOrdering::row){
-        if(is_compress()){
+    
+    if(is_compress()){
+        if constexpr(S==StorageOrdering::row){
             for(unsigned int jj=RowPoint[i]; jj<RowPoint[i+1]; jj++){
                 if(ColIndx[jj]==j)
                     return val[jj];     
             }
             return 0;
         }
-        if(Dati.find({i,j}) != Dati.end())
-            return Dati.at({i,j});
-        else
-            return 0;
-    }
-    if(S==StorageOrdering::col){
-        if(is_compress()){
+        if constexpr(S==StorageOrdering::col){
             for(unsigned int jj=RowPoint[j]; jj<RowPoint[j+1]; jj++){
                 if(ColIndx[jj]==i)
                     return val[jj];     
             }
-            return 0;
+           return 0;
         }
-        if(Dati.find({i,j}) != Dati.end()){
-            return Dati.at({i,j});}
-        else
-            return 0;
     }
-
-
+    if(Dati.find({i,j}) != Dati.end())
+        return Dati.at({i,j});
+    else
+        return 0;
 }
 
 // metodo che estrae riga k
 template <class T,StorageOrdering S>
 std::map<std::array<std::size_t,2>,T> Matrix<T,S>::estrai(const std::size_t k){
     std::map<std::array<std::size_t,2>,T> riga; //map contenente riga K/o colonna
-    if(S==StorageOrdering::row){
+    if constexpr(S==StorageOrdering::row){
         for (auto it=Dati.lower_bound({k,0}); it!=Dati.lower_bound({k+1,0}); it++){
             riga[it->first]=it->second;
         }
         return riga;}
-    if(S==StorageOrdering::col){  
-                for (auto it=Dati.upper_bound({0,k}); it!=Dati.upper_bound({0,k+1}); it++){
+    if constexpr(S==StorageOrdering::col){  
+        for (auto it=Dati.lower_bound({0,k}); it!=Dati.lower_bound({0,k+1}); it++){
             riga[it->first]=it->second;
         }
         return riga;}
@@ -87,36 +74,29 @@ std::map<std::array<std::size_t,2>,T> Matrix<T,S>::estrai(const std::size_t k){
 // comprime dati-->vectors
 template <class T,StorageOrdering S>
 void Matrix<T,S>::compress(){
-    if(S==StorageOrdering::row){
-        unsigned int nrow = (Dati.rbegin())->first[0];//--Dati.end() rida it a ultimo elemento di map, (--Dati.end())->first[0] rida numero di righe di matrice
+    int a;
+    int b;
+    if constexpr(S==StorageOrdering::row){
+        a=1;
+        b=0;
+    }
+    if constexpr(S==StorageOrdering::col){
+        a=0;
+        b=0;
+    }
+        unsigned int n = (Dati.rbegin())->first[b];//numero righe o colonne
         unsigned int point = 0; //per inserire posizione di primo elemento non vuoto in riga 
         RowPoint.push_back(point); //primo elemento sempre 0;
-        for (unsigned int i=0; i<=nrow; i++){   // <= perchè in Dati.first indice riga è gia indice che parte da 0
+        for (unsigned int i=0; i<=n; i++){   // <= perchè in Dati.first indice riga/colonna è gia indice che parte da 0
             auto R=estrai(i);
             for (auto it=R.begin(); it!=R.end(); it++){
                 val.push_back(it->second);
-                ColIndx.push_back(it->first[1]);
+                ColIndx.push_back(it->first[a]);
                 point++;
                 }
             RowPoint.push_back(point);
         }
         Dati.clear();//svuotata map di dati una volta passati da dinamic a CSR
-    }
-    if(S==StorageOrdering::col){
-        unsigned int ncol = (Dati.rbegin())->first[0];//--Dati.end() rida it a ultimo elemento di map, (--Dati.end())->first[0] rida numero di colonna di matrice
-        unsigned int point = 0; //per inserire posizione di primo elemento non vuoto in riga 
-        RowPoint.push_back(point); //primo elemento sempre 0;
-        for (unsigned int i=0; i<=ncol; i++){   // <= perchè in Dati.first indice riga è gia indice che parte da 0
-            auto R=estrai(i);
-            for (auto it=R.begin(); it!=R.end(); it++){
-                val.push_back(it->second);
-                ColIndx.push_back(it->first[0]);
-                point++;
-                }
-            RowPoint.push_back(point);
-        }
-        Dati.clear();//svuotata map di dati una volta passati da dinamic a CSR
-    }
 }
 
 template <class T,StorageOrdering S>
--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 using namespace algebra;
 
 int main(){
-/*   // prova constructor
+  // prova constructor
    // per righe---------> ok (compila) 
     std::map<std::array<std::size_t,2>,int,cmp<StorageOrdering::row>> mapR={{{0,0},1},{{0,1},2},{{1,0},3},{{1,1},4}};
     Matrix<int,StorageOrdering::row> MR(mapR); 
@@ -13,11 +13,11 @@ int main(){
     std::map<std::array<std::size_t,2>,int,cmp<StorageOrdering::col>> mapC={{{0,0},1},{{0,1},3},{{1,0},2},{{1,1},4}};
     //std::map<std::array<std::size_t,2>,int> mapC={{{0,0},1},{{0,1},4},{{1,1},2},{{1,2},5},{{2,2},3},{{2,3},6}};
     Matrix<int,StorageOrdering::col> MC(mapC);
-*/
-/*
-  //estri--------->ok
+
+
+ /* //estri--------->ok
     //righe
-    std::map<std::array<std::size_t,2>,int> DR(MR.estrai(1));
+    std::map<std::array<std::size_t,2>,int> DR(MR.estrai(0));
     for(auto it= DR.begin(); it!=DR.end();it++){
         std::cout<<it->first[0]<<" "<<it->first[1]<<" value: "<<it->second<<std::endl;
     }
@@ -27,7 +27,7 @@ int main(){
         std::cout<<it->first[0]<<" "<<it->first[1]<<" value: "<<it->second<<std::endl;
     }
 */
-/*    
+    
 
     // compress(),uncompress(),is_compress() per R--->ok 
     std::cout<<"è compressa (0): "<<MR.is_compress()<<std::endl;
@@ -48,7 +48,7 @@ int main(){
     MC.printvett();
 
 
-    // call operator righe
+ /*   // call operator righe
     // non const------>ok 
     Matrix<int,StorageOrdering::row> NR; //+ defaul const
     NR(0,0)=1;
@@ -60,8 +60,8 @@ int main(){
     NR(1,1)=5;
     NR.printvett();
     
-*/
-/*    //call operator colonne     
+
+    //call operator colonne     
     // non const------>ok
     Matrix<int,StorageOrdering::col> NC; //+ defaul const
     NC(0,0)=1;
@@ -73,7 +73,7 @@ int main(){
     NC(1,1)=5;
     NC.printvett();
 */
-  // call op const
+/*  // call op const
     // per righe---->ok
     std::map<std::array<std::size_t,2>,int,cmp<StorageOrdering::row>> mapR={{{0,0},1},{{0,1},2},{{1,1},4}};
     const Matrix<int,StorageOrdering::row> MR(mapR); 
@@ -83,7 +83,7 @@ int main(){
     //std::map<std::array<std::size_t,2>,int> mapC={{{0,0},1},{{0,1},4},{{1,1},2},{{1,2},5},{{2,2},3},{{2,3},6}};
     const Matrix<int,StorageOrdering::col> MC(mapC);
     std::cout<<"m(0,0)= 1:"<<MC(0,0)<<"m(10,10)=0 :"<<MC(10,10)<<std::endl;
-
+*/
     std::cout<<MR(1,0);
     std::cout<<MC(1,0);
 
